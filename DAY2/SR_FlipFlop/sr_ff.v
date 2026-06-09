d30a39dd6f80f87ffdae26c9aba0e73bda4f3bd1
Ash-809 <ashwinnair.nandanam@gmail.com>
2026-06-09 21:15:41 +0530

    Update sr_ff.v

--- DAY2/SR_FlipFlop/sr_ff.v
+++ DAY2/SR_FlipFlop/sr_ff.v
@@ -1,1 +1,24 @@
+`timescale 1ns / 1ps
+module sr_ff(input clk,
+    input rst,
+    input s,
+    input r,
+    output reg q,
+    output q_bar
+);
 
+    assign q_bar = ~q;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            q <= 1'b0;
+        end else begin
+            case ({s, r})
+                2'b00: q <= q;
+                2'b01: q <= 1'b0;
+                2'b10: q <= 1'b1;
+                2'b11: q <= 1'b0;
+            endcase
+        end
+    end
+endmodule
