47dc589c2b84f9cdeae059bf3eb51a275214d13d
Ash-809 <ashwinnair.nandanam@gmail.com>
2026-06-09 21:24:01 +0530

    Update tb_d_ff.v

--- DAY2/D_Flip-Flop/tb_d_ff.v
+++ DAY2/D_Flip-Flop/tb_d_ff.v
@@ -1,1 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 18:14:37
+// Design Name: 
+// Module Name: tb_d_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module tb_d_ff;
+reg clk;
+    reg rst;
+    reg d;
+
+    wire q;
+    wire q_bar;
+
+    dff uut (
+        .clk(clk),
+        .rst(rst),
+        .d(d),
+        .q(q),
+        .q_bar(q_bar)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; 
+    end
+
+    initial begin
+        rst = 1; d = 0;
+        #20; 
+        
+        rst = 0; 
+        
+        #15 d = 1;
+        
+        #10 d = 0;
+        
+        #20 d = 1;
+        
+        #10 rst = 1;
+        
+        #10 rst = 0;
+
+        #20 $finish;
+    end
+endmodule
