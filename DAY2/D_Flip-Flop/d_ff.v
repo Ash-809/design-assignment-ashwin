e9026647780dedde724e69db28a89c5b966b5969
Ash-809 <ashwinnair.nandanam@gmail.com>
2026-06-09 21:23:01 +0530

    Update d_ff.v

--- DAY2/D_Flip-Flop/d_ff.v
+++ DAY2/D_Flip-Flop/d_ff.v
@@ -1,1 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 18:12:46
+// Design Name: 
+// Module Name: dff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module dff(input clk,
+    input rst,
+    input d,
+    output reg q,
+    output q_bar
+);
+
+    assign q_bar = ~q;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            q <= 1'b0;
+        end else begin
+            q <= d;
+        end
+    end
+endmodule
