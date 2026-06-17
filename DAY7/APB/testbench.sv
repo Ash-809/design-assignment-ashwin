3de6b27c6cea6c877cf4ffd2a605d0d0b02c7e1d
Ash-809 <ashwinnair.nandanam@gmail.com>
2026-06-17 21:21:14 +0530

    Create testbench.sv

---
+++ DAY7/APB/testbench.sv
@@ -0,0 +1,52 @@
+`include "interface.sv"
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
+`include "test.sv"
+
+module testbench;
+    logic pclk;
+    logic presetn;
+
+    
+    always #5 pclk = ~pclk;
+
+  
+    apb_if intf(pclk, presetn);
+
+    
+    apb_slave dut (
+        .pclk(intf.pclk),
+        .presetn(intf.presetn),
+        .paddr(intf.paddr),
+        .pwrite(intf.pwrite),
+        .psel(intf.psel),
+        .penable(intf.penable),
+        .pwdata(intf.pwdata),
+        .prdata(intf.prdata),
+        .pready(intf.pready),
+        .pslverr(intf.pslverr)
+    );
+
+    test t1;
+
+    initial begin
+        pclk = 0;
+        presetn = 0;
+        
+       
+        #15 presetn = 1;
+        
+        t1 = new(intf);
+        t1.run();
+    end
+
+    
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars;
+    end
+endmodule
